9f13fcd2b839fb52fe4741896dc3261c5b810f47
GeneralReznov <mokshitkaushik21@gmail.com>
2026-07-01 16:45:36 +0530

    Fix indentation in uvicorn.run call

--- main.py
+++ main.py
@@ -236,5 +236,4 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(
-        "main:app", host="0.0.0.0", port=int(os.getenv("PORT", 5000)), reload=True
-    )
+        "main:app", host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
